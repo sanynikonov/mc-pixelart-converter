7770b8d4aa2b8f680a434bfa38f7abce475a8416
sanynikonov <nikonov.sanynikonov@gmail.com>
2021-01-23 17:31:26 +0200

    Adds two common strategies

---
+++ PixelartGenerator.Entities/NearestColorStrategies/CommonNearestNeighbourStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace PixelartGenerator.Entities
+{
+    public class CommonNearestNeighbourStrategy : INearestColorStrategy
+    {
+        private static CustomNearestColorStrategy _strategy = new CustomNearestColorStrategy(1, 1, 1);
+
+        public double DistanceToNeighbour(Color color, Color neighbour)
+        {
+            return _strategy.DistanceToNeighbour(color, neighbour);
+        }
+    }
+}
---
+++ PixelartGenerator.Entities/NearestColorStrategies/HumanPerceptionNearestColorStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace PixelartGenerator.Entities
+{
+    public class HumanPerceptionNearestColorStrategy : INearestColorStrategy
+    {
+        private static CustomNearestColorStrategy _strategy = new CustomNearestColorStrategy(30, 59, 11);
+
+        public double DistanceToNeighbour(Color color, Color neighbour)
+        {
+            return _strategy.DistanceToNeighbour(color, neighbour);
+        }
+    }
+}
