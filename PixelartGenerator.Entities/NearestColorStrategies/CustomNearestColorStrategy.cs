3de5c383daf878ad3d082ac295bf1fc55cdb2b2c
sanynikonov <nikonov.sanynikonov@gmail.com>
2021-01-23 17:31:03 +0200

    Creates strategy to find nearest neighbour

---
+++ PixelartGenerator.Entities/NearestColorStrategies/CustomNearestColorStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace PixelartGenerator.Entities
+{
+    public class CustomNearestColorStrategy : INearestColorStrategy
+    {
+        private double _rWeight;
+        private double _bWeight;
+        private double _gWeight;
+
+        public CustomNearestColorStrategy(double rWeight, double gWeight, double bWeight)
+        {
+            _rWeight = rWeight;
+            _bWeight = bWeight;
+            _gWeight = gWeight;
+        }
+
+        public double DistanceToNeighbour(Color c1, Color c2)
+        {
+            return _rWeight * Math.Pow(c2.R - c1.R, 2)
+                + _gWeight * Math.Pow(c2.G - c1.G, 2)
+                + _bWeight * Math.Pow(c2.B - c1.B, 2);
+        }
+    }
+}
---
+++ PixelartGenerator.Entities/NearestColorStrategies/INearestColorStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace PixelartGenerator.Entities
+{
+    public interface INearestColorStrategy
+    {
+        double DistanceToNeighbour(Color color, Color neighbour);
+    }
+}
